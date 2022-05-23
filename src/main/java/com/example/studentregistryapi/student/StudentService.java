46090e1d481d14590f92a1931e3f6db2c60a6a8f
David Rose <daviderose526@gmail.com>
2022-05-23 15:21:38 -0500

    added ability to update students

--- src/main/java/com/example/studentregistryapi/student/StudentService.java
+++ src/main/java/com/example/studentregistryapi/student/StudentService.java
@@ -1,14 +1,16 @@
 package com.example.studentregistryapi.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
 	
-	private final StudentRepository studentRepository;
+	public final StudentRepository studentRepository;
 	
 	public StudentService(StudentRepository studentRepository) {
 		this.studentRepository = studentRepository;
@@ -27,4 +29,40 @@ public class StudentService {
 		}
 		studentRepository.save(student);		
 	}
+
+	public void deleteStudent(Long studentId) {
+		
+		boolean exists = studentRepository.existsById(studentId);
+		if (!exists) {
+			throw new IllegalStateException(
+					"student with id " + studentId + " does not exist."
+					);
+		}
+		
+		studentRepository.deleteById(studentId);	
+	}
+	
+	@Transactional
+	public void updateStudent(Long studentId,
+							String name,
+							String email) {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new IllegalStateException(
+						"student with id " + studentId + " does not exist."
+						));
+	
+		if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		
+		if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("email is taken.");
+			}
+			student.setEmail(email);
+		}
+	}	
 }
+
