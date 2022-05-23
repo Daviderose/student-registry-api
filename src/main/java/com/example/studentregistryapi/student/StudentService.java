9560fa74b31d2b8ad2e4cb128b253e00624c006a
David Rose <daviderose526@gmail.com>
2022-05-23 09:15:05 -0500

    fixing gitignore

--- src/main/java/com/example/studentregistryapi/student/StudentService.java
+++ src/main/java/com/example/studentregistryapi/student/StudentService.java
@@ -1,22 +1,18 @@
 package com.example.studentregistryapi.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 	
-	public List<Student> getStudents() {
-		return Stream.of(
-				new Student(1L, "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000,  Month.JANUARY, 5 ), 21)
-				
-				)
-				.collect(Collectors.toList());
+	private final StudentRepository studentRepository;
+	
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
 	}
 
+	public List<Student> getStudents() {
+		return studentRepository.findAll();
+	}
 }
